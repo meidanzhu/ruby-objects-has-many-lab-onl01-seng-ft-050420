057e6007447f82cd7b7d12b8efeb438cb4f36a8c
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-18 20:59:15 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,24 @@
 class Author
   attr_accessor :name
-  @@post_count = 0
+
+  @@all = []
 
   def initialize(name)
     @name = name
-    @posts = []
+    @@all << self
   end
 
-  def posts
-    @posts
+  def self.all
+    @@all
   end
 
   def add_post(post)
-    @posts << post
     post.author = self
-    @@post_count += 1
   end
 
-  def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    @posts << post
+  def add_post_by_title(title)
+    post = Post.new(title)
     post.author = self
-    @@post_count += 1
   end
 
   def self.post_count
