90518f01e8004435c860ac2eab170cd4c932a2df
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-18 20:47:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,16 +7,16 @@ class Artist
     @songs = []
   end
 
-  def songs
-    @songs
-  end
-
   def add_song(song)
     @songs << song
     song.artist = self
     @@song_count += 1
   end
 
+  def songs
+    @songs
+  end
+  
   def add_song_by_name
     song = song.new(song_name)
     @songs << song
