1b2fc7cdd2997833d011f99f31ac992ec6802b6e
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-18 20:59:54 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,15 @@
 class Post
   attr_accessor :author, :title
 
+  @@all = []
+
   def initialize(title)
     @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def author_name
