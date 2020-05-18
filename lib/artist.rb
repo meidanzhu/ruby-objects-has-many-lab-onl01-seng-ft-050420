12d38356608c014b71b585ab1e5fe53fe828027a
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-18 20:49:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,5 @@
 class Artist
+
   attr_accessor :name, :songs
   @@song_count = 0
 
@@ -7,18 +8,14 @@ class Artist
     @songs = []
   end
 
-  def songs
-    @songs
-  end
-
   def add_song(song)
     @songs << song
     song.artist = self
     @@song_count += 1
   end
 
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song
     song.artist = self
     @@song_count += 1
